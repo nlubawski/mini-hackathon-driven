bbcf4dfa53314cc44776f1832762e9a2d13809dd
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-04 19:50:46 -0300

    feat: criada renderizacao do resultado das buscas

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,6 @@ navigator.geolocation.getCurrentPosition((position) => {
     buscaPorGeolocalizao(position.coords.latitude, position.coords.longitude);
 });
 
-// function coordenadas(latitude, longitude){
-//     console.log(latitude, longitude)
-//     buscaPorGeolocalizao(latitude, longitude)
-// }
-
 function buscaPorGeolocalizao(latitude, longitude){
     let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric&lang=pt_br`)
     promisse.then(sucessoGeolocalizao)
@@ -21,16 +16,30 @@ function sucessoGeolocalizao(resposta){
     renderizaGeolocalizacao(local)
 }
 
-
 function renderizaGeolocalizacao(local){
+    let texto = document.querySelector('.resultado')
+    texto.innerHTML = `
+        <div class="nome">${local.name}</div>
+        <div class="informacoes">
+            <div class="temperatura">
+                <div>Temperatura</div>
+                <div>${local.main["temp"]} °C</div>
+            </div>
+            <div class="condicoes">
+                <div>${local.weather[0].main}</div>
+                <div>${local.weather[0].description}</div>
 
+            </div>
+        </div>
+    `
 }
 
-function buscaPorNomeOuCep(){
-    entrada = "Massaranduba"
+function buscaPorNomeOuCep(event){
+    event.preventDefault()
+    entrada = document.querySelector('input').value
     //entradaCEP = "17021" ${encodeURI(entrada)},
     let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(entrada)}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric&lang=pt_br`)
-    promisse.then(resposta => console.log(resposta.data))
+    promisse.then(resposta => {renderizaGeolocalizacao(resposta.data), document.querySelector('input').value = ''})
 }
 
-buscaPorNomeOuCep()
+
