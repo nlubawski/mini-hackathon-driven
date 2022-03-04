c043452931689e0073cee7695f0597d3213f9fad
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-04 20:25:15 -0300

    feat: layout desktop criado, ajustes gerais

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,9 @@
-// cc5be94da6c41fb61c71bc297b10ad ce
-
 navigator.geolocation.getCurrentPosition((position) => {
     buscaPorGeolocalizao(position.coords.latitude, position.coords.longitude);
 });
 
 function buscaPorGeolocalizao(latitude, longitude){
-    let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric&lang=pt_br`)
+    let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=cc5be94da6c41fb61c71bc297b10adce&units=metric&lang=pt_br`)
     promisse.then(sucessoGeolocalizao)
     promisse.catch(erro => console.log(erro))
 }
@@ -22,14 +20,11 @@ function renderizaNaTela(local){
         <div class="nome">${local.name}</div>
         <div class="informacoes">
             <div class="temperatura">
-                <div>Temperatura</div>
-                <div>${local.main["temp"]} °C</div>
+                <div>Temperatura: ${local.main["temp"]} °C</div>
                 <img src="http://openweathermap.org/img/wn/${local.weather[0].icon}.png" />
             </div>
             <div class="condicoes">
-                
                 <div>${local.weather[0].description}</div>
-
             </div>
         </div>
     `
@@ -39,7 +34,7 @@ function buscaPorNomeOuCep(event){
     event.preventDefault()
     entrada = document.querySelector('input').value
     //entradaCEP = "17021" ${encodeURI(entrada)},
-    let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(entrada)}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric&lang=pt_br`)
+    let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(entrada)}&appid=cc5be94da6c41fb61c71bc297b10adce&units=metric&lang=pt_br`)
     promisse.then(resposta => {renderizaNaTela(resposta.data), document.querySelector('input').value = ''})
 }
 
