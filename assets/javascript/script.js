c721c2e04d25e2b4c38389d0f8f1cdca950ff443
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-04 18:37:31 -0300

    feat: criada buscaPorGeolocalizao com sucesso usando a chave de teste

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,27 @@
+// cc5be94da6c41fb61c71bc297b10ad ce
+
 navigator.geolocation.getCurrentPosition((position) => {
-    coordenadas(position.coords.latitude, position.coords.longitude);
+    buscaPorGeolocalizao(position.coords.latitude, position.coords.longitude);
 });
 
+// function coordenadas(latitude, longitude){
+//     console.log(latitude, longitude)
+//     buscaPorGeolocalizao(latitude, longitude)
+// }
 
-function coordenadas(latitude, longitude){
-    console.log(latitude, longitude)
+function buscaPorGeolocalizao(latitude, longitude){
+    let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f25110b0f83adb9f7c080ee182cd1d00`)
+    promisse.then(sucessoGeolocalizao)
+    promisse.catch(erro => console.log(erro))
+}
+
+function sucessoGeolocalizao(resposta){
+    console.log(resposta.data)
+    let local = resposta.data
+    renderizaGeolocalizacao(local)
+}
+
+
+function renderizaGeolocalizacao(local){
+    
 }
