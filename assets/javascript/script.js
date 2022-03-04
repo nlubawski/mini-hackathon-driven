d35bd7ecc2bd39ae6cf3dd7a13299a3e56cddff8
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-04 20:00:50 -0300

    fix: nome da funcao de renderizar por geolocalizao foi modificada pra renderizaNaTela, correçoes e alteraçoes no html renderizado

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,10 +13,10 @@ function buscaPorGeolocalizao(latitude, longitude){
 function sucessoGeolocalizao(resposta){
     console.log(resposta.data)
     let local = resposta.data
-    renderizaGeolocalizacao(local)
+    renderizaNaTela(local)
 }
 
-function renderizaGeolocalizacao(local){
+function renderizaNaTela(local){
     let texto = document.querySelector('.resultado')
     texto.innerHTML = `
         <div class="nome">${local.name}</div>
@@ -24,9 +24,10 @@ function renderizaGeolocalizacao(local){
             <div class="temperatura">
                 <div>Temperatura</div>
                 <div>${local.main["temp"]} °C</div>
+                <img src="http://openweathermap.org/img/wn/${local.weather[0].icon}.png" />
             </div>
             <div class="condicoes">
-                <div>${local.weather[0].main}</div>
+                
                 <div>${local.weather[0].description}</div>
 
             </div>
@@ -39,7 +40,7 @@ function buscaPorNomeOuCep(event){
     entrada = document.querySelector('input').value
     //entradaCEP = "17021" ${encodeURI(entrada)},
     let promisse = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(entrada)}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric&lang=pt_br`)
-    promisse.then(resposta => {renderizaGeolocalizacao(resposta.data), document.querySelector('input').value = ''})
+    promisse.then(resposta => {renderizaNaTela(resposta.data), document.querySelector('input').value = ''})
 }
 
 
